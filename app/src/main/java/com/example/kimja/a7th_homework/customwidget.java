30b440c1a695166ea44d7ad275d1b7286f4d33a4
hk1459 <gusrbdia66@naver.com>
2017-05-03 19:28:49 +0900

    실습과제 진행중 -체크박스 구현 -커스텀위젯 add기능 구현 -수정기능 구현 -autocompletetextview 구현

--- app/src/main/java/com/example/kimja/a7th_homework/customwidget.java
+++ app/src/main/java/com/example/kimja/a7th_homework/customwidget.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -12,13 +14,16 @@ import android.widget.LinearLayout;
 
 public class customwidget extends LinearLayout implements View.OnClickListener {
     static int imageno= 1;
-    static EditText et,et2;
+    static EditText et2;
     static ImageView img;
     Button b_next;
     static Button b_add;
+    static AutoCompleteTextView et;
     final int[] imagelist = {R.drawable.abocado,R.drawable.banana
             ,R.drawable.cherry,R.drawable.cranberry,R.drawable.grape,R.drawable.kiwi,R.drawable.orange
             ,R.drawable.watermelon};
+    String[] names = new String[]{"아보카도", "바나나","체리","크랜베리","포도","키위","오렌지","수박"
+    ,"abocado","banana","cherry","cranberry","grape","kiwi","orange","watermelon"};
 
     public customwidget (Context context , AttributeSet attrs){
         super(context , attrs);
@@ -27,13 +32,18 @@ public class customwidget extends LinearLayout implements View.OnClickListener {
 
     void init( Context context){
         LayoutInflater.from(context).inflate(R.layout.fruitadd,this);
-        et = (EditText)findViewById(R.id.f_name); // autocomplete
+         // autocomplete
         et2 = (EditText)findViewById(R.id.f_price);
         img = (ImageView)findViewById(R.id.image1);
         b_next = (Button)findViewById(R.id.b_next);
         b_add = (Button)findViewById(R.id.b_add);
         b_next.setOnClickListener(this);
         b_add.setOnClickListener(this);
+
+        ArrayAdapter<String> name = new ArrayAdapter<String>(context,android.R.layout.simple_dropdown_item_1line,names);
+        et = (AutoCompleteTextView) findViewById(R.id.f_name);
+        et.setAdapter(name);
+
     }
     interface  OnAddListener {
         void onAdd(String name, int imageno, int price);
